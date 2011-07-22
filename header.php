33f1a680bbf9628d19fb5d7fde650c7b9ce567b7
Hamish Taplin <hello@hamishtaplin.com>
2011-07-22 21:40:55 +0100

    Initial commit
    First commit of theme files.

---
+++ header.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+ 
+    global $page, $paged;
+ 
+    wp_title( '|', true, 'right' );
+ 
+    bloginfo( 'name' );
+ 
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+        echo " | $site_description";
+ 
+    if ( $paged >= 2 || $page >= 2 )
+        echo ' | ' . sprintf( __( 'Page %s', 'starkers' ), max( $paged, $page ) );
+ 
+    ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+ 
+<script src="<?php bloginfo('template_directory'); ?>/js/modernizr-1.6.min.js"></script>
+ 
+<?php
+    /* We add some JavaScript to pages with the comment form
+     * to support sites with threaded comments (when in use).
+     */
+    if ( is_singular() && get_option( 'thread_comments' ) )
+        wp_enqueue_script( 'comment-reply' );
+ 
+    /* Always have wp_head() just before the closing </head>
+     * tag of your theme, or you will break many plugins, which
+     * generally use this hook to add elements to <head> such
+     * as styles, scripts, and meta tags.
+     */
+    wp_head();
+?>
+</head>
+ 
+<body <?php body_class(); ?>>
+ 
+    <header>
+        <hgroup>
+            <h1><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <h2><?php bloginfo( 'description' ); ?></h2>
+        </hgroup>
+ 
+        <?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to the 'starkers_menu' function which can be found in functions.php.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
+        <?php wp_nav_menu( array( 'container' => 'nav', 'fallback_cb' => 'starkers_menu', 'theme_location' => 'primary' ) ); ?>
+    </header>
---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+
+Author:
+Date: 
+Project:
+
+*/
+
+"use strict";
+
+//Closure to prevent global scope.
+
+(function(){
+
+	// Javascript
+	
+}());
+
+
